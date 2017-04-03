f6f152573f1e83b1ca977b38d1527375d26d9001
Juan Plaza <juanplaza@juans-macbook-pro.local>
2017-04-03 16:26:56 -0400

    Initial Commit

---
+++ JuanPlaza/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuanPlaza
+//
+//  Created by Juan Plaza on 4/3/17.
+//  Copyright © 2017 Juan Plaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
