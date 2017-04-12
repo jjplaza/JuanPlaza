9826198ca5cedbb596da9b9887654efe2c63664c
Juan Plaza <juanplaza@juans-macbook-pro.local>
2017-04-11 22:24:11 -0400

    April 11, 2017 Version 1.10 -Added text field validation when adding vehicle. -Added a 'Help' navigation bar button in SetVehicle that pops up an alert to show user how to proceed to view.

--- JuanPlaza/SetVehicle.swift
+++ JuanPlaza/SetVehicle.swift
@@ -16,15 +16,45 @@ class SetVehicle: UIViewController {
     @IBOutlet weak var carModel: UITextField!
     @IBOutlet weak var carTrim: UITextField!
     @IBOutlet weak var submitButton: UIButton!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.barTintColor = UIColor.white
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        submitButton.isEnabled = false
+        vehicleYear.keyboardType = UIKeyboardType.numberPad
+        
+    
     }
     
+    @IBAction func helpPop(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Help", message: "You must enter car year, make, and model.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func checkCar() {
+        if (vehicleYear.text!.characters.count == 4 && !carMake.text!.isEmpty && !carModel.text!.isEmpty) {
+            submitButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func checkYear(_ sender: UITextField) {
+        
+        checkCar()
+    }
+    
+    @IBAction func checkModel(_ sender: UITextField) {
+        
+        checkCar()
+    }
+    
+    @IBAction func checkMake(_ sender: UITextField) {
+        
+        checkCar()
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -42,6 +72,7 @@ class SetVehicle: UIViewController {
         let uMake = carMake.text!
         let uModel = carModel.text!
         let uTrim = carTrim.text!
+        
         
         if (segue.identifier == "goToServices") {
             let showvehicle = segue.destination as? Services
